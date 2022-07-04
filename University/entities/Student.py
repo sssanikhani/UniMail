b1546745358eb8a6755cbb0459969d5e7fcf2d54
Sajjad <sssanikhani@gmail.com>
2022-07-04 18:07:32 +0430

    fix: change cls field of Student to class_id

--- University/entities/Student.py
+++ University/entities/Student.py
@@ -7,5 +7,5 @@ from University.entities.Class import Class
 class Student(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     email = models.CharField(max_length=100, null=False, blank=False)
-    cls = models.ForeignKey(to=Class, on_delete=models.deletion.SET_NULL, null=True)
+    class_id = models.ForeignKey(to=Class, on_delete=models.deletion.SET_NULL, null=True)
     score = models.FloatField(validators=[MaxValueValidator(20), MinValueValidator(0)])
